0ec93eb3f6a3501ba2f9dbcb718ee2fbf929841b
ucii88 <suci.sulistiawati.tif24@polban.ac.id>
2026-02-11 10:23:31 +0700

    fix: perbaikan input step dan penyesuaian tombol saat keyboard muncul

--- lib/counter_view.dart
+++ lib/counter_view.dart
@@ -10,167 +10,191 @@ class CounterView extends StatefulWidget {
 
 class _CounterViewState extends State<CounterView> {
   final CounterController controller = CounterController();
+  final TextEditingController stepController = TextEditingController();
+
+  @override
+  void dispose() {
+    stepController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: const Color.fromARGB(255, 176, 164, 246),
-        title: const Text(
-          "Logbook : SRP version",
-          style: TextStyle(color: Color.fromARGB(255, 79, 30, 152)),
-        ),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(20),
-        child: Column(
-          children: [
-            const SizedBox(height: 120),
-            Card(
-              elevation: 3,
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(14),
-              ),
-              child: Padding(
-                padding: const EdgeInsets.all(16),
-                child: Column(
-                  children: [
-                    TextField(
-                      keyboardType: TextInputType.number,
-                      decoration: const InputDecoration(
-                        labelText: "Masukkan Step",
-                        border: OutlineInputBorder(),
-                      ),
-                      onChanged: (value) {
-                        setState(() {
-                          controller.setStep(int.tryParse(value) ?? 1);
-                        });
-                      },
-                    ),
-
-                    const SizedBox(height: 16),
-
-                    const Text(
-                      "Total Hitungan",
-                      style: TextStyle(fontSize: 16),
-                    ),
-                    const SizedBox(height: 6),
-
-                    Text(
-                      '${controller.value}',
-                      style: Theme.of(context).textTheme.headlineLarge
-                          ?.copyWith(fontWeight: FontWeight.bold),
-                    ),
-                  ],
-                ),
-              ),
+    return GestureDetector(
+      onTap: () => FocusScope.of(context).unfocus(),
+      child: Scaffold(
+        appBar: AppBar(
+          backgroundColor: const Color.fromARGB(255, 176, 164, 246),
+          centerTitle: true,
+          title: const Text(
+            "Logbook : SRP version",
+            style: TextStyle(
+              color: Color.fromARGB(255, 79, 30, 152),
+              fontWeight: FontWeight.bold,
             ),
-
-            const SizedBox(height: 20),
-            const Divider(),
-            const Text(
-              "Riwayat Aktivitas",
-              style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
-            ),
-            const SizedBox(height: 10),
-
-            Flexible(
-              child: controller.history.isEmpty
-                  ? Padding(
-                      padding: const EdgeInsets.only(top: 16),
-                      child: Text(
-                        "Belum ada aktivitas",
-                        style: TextStyle(color: Colors.grey),
-                      ),
-                    )
-                  : ListView.builder(
-                      itemCount: controller.history.length,
-                      itemBuilder: (context, index) {
-                        final historyText = controller.history[index];
-
-                        Color textColor = Colors.black;
-                        if (historyText.contains("bertambah")) {
-                          textColor = Colors.green;
-                        } else if (historyText.contains("berkurang")) {
-                          textColor = Colors.red;
-                        }
-                        return Padding(
-                          padding: const EdgeInsets.symmetric(vertical: 4),
-                          child: Text(
-                            "- $historyText",
-                            style: TextStyle(color: textColor),
-                          ),
-                        );
-                      },
-                    ),
-            ),
-          ],
-        ),
-      ),
-
-      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
-
-      floatingActionButton: Row(
-        mainAxisSize: MainAxisSize.min,
-        mainAxisAlignment: MainAxisAlignment.end,
-        children: [
-          FloatingActionButton(
-            heroTag: "decrement",
-            backgroundColor: Colors.red,
-            onPressed: () {
-              setState(() {
-                controller.decrement();
-              });
-            },
-            child: const Icon(Icons.remove),
           ),
-          const SizedBox(width: 12),
+        ),
+        body: Padding(
+          padding: const EdgeInsets.all(20),
+          child: Column(
+            children: [
+              const SizedBox(height: 60),
+              Card(
+                elevation: 3,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(14),
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.all(16),
+                  child: Column(
+                    children: [
+                      TextField(
+                        controller: stepController,
+                        keyboardType: TextInputType.number,
+                        textInputAction: TextInputAction.done,
+                        decoration: const InputDecoration(
+                          labelText: "Masukkan Step",
+                          border: OutlineInputBorder(),
+                        ),
 
-          FloatingActionButton(
-            heroTag: "reset",
-            onPressed: () {
-              showDialog(
-                context: context,
-                builder: (context) {
-                  return AlertDialog(
-                    title: Text("Konfirmasi"),
-                    content: Text("Yakin ingin reset?"),
-                    actions: [
-                      TextButton(
-                        onPressed: () {
-                          Navigator.pop(context);
+                        onChanged: (value) {
+                          controller.setStep(int.tryParse(value) ?? 1);
                         },
-                        child: Text("Batal"),
+
+                        onSubmitted: (_) {
+                          FocusScope.of(context).unfocus();
+                        },
                       ),
-                      TextButton(
-                        onPressed: () {
-                          setState(() {
-                            controller.reset();
-                          });
-                          Navigator.pop(context);
-                        },
-                        child: Text("Reset"),
+
+                      const SizedBox(height: 16),
+
+                      const Text(
+                        "Total Hitungan",
+                        style: TextStyle(fontSize: 16),
+                      ),
+                      const SizedBox(height: 6),
+
+                      Text(
+                        '${controller.value}',
+                        style: Theme.of(context).textTheme.headlineLarge
+                            ?.copyWith(fontWeight: FontWeight.bold),
                       ),
                     ],
+                  ),
+                ),
+              ),
+
+              const SizedBox(height: 20),
+              const Divider(),
+              const Text(
+                "Riwayat Aktivitas",
+                style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+              ),
+              const SizedBox(height: 10),
+
+              Flexible(
+                child: controller.history.isEmpty
+                    ? Padding(
+                        padding: const EdgeInsets.only(top: 16),
+                        child: Text(
+                          "Belum ada aktivitas",
+                          style: TextStyle(color: Colors.grey),
+                        ),
+                      )
+                    : ListView.builder(
+                        itemCount: controller.history.length,
+                        itemBuilder: (context, index) {
+                          final historyText = controller.history[index];
+
+                          Color textColor = Colors.black;
+                          if (historyText.contains("bertambah")) {
+                            textColor = Colors.green;
+                          } else if (historyText.contains("berkurang")) {
+                            textColor = Colors.red;
+                          }
+                          return Padding(
+                            padding: const EdgeInsets.symmetric(vertical: 4),
+                            child: Text(
+                              "- $historyText",
+                              style: TextStyle(color: textColor),
+                            ),
+                          );
+                        },
+                      ),
+              ),
+            ],
+          ),
+        ),
+
+        floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+
+        floatingActionButton: AnimatedPadding(
+          duration: const Duration(milliseconds: 200),
+          padding: EdgeInsets.only(
+            bottom: MediaQuery.of(context).viewInsets.bottom > 0 ? 20 : 0,
+          ),
+          child: Row(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              FloatingActionButton(
+                heroTag: "decrement",
+                backgroundColor: Colors.red,
+                onPressed: () {
+                  setState(() {
+                    controller.decrement();
+                  });
+                },
+                child: const Icon(Icons.remove),
+              ),
+              const SizedBox(width: 12),
+
+              FloatingActionButton(
+                heroTag: "reset",
+                onPressed: () {
+                  showDialog(
+                    context: context,
+                    builder: (context) {
+                      return AlertDialog(
+                        title: Text("Konfirmasi"),
+                        content: Text("Yakin ingin reset?"),
+                        actions: [
+                          TextButton(
+                            onPressed: () {
+                              Navigator.pop(context);
+                            },
+                            child: Text("Batal"),
+                          ),
+                          TextButton(
+                            onPressed: () {
+                              setState(() {
+                                controller.reset();
+                              });
+                              Navigator.pop(context);
+                            },
+                            child: Text("Reset"),
+                          ),
+                        ],
+                      );
+                    },
                   );
                 },
-              );
-            },
-            child: const Icon(Icons.refresh),
-          ),
-          const SizedBox(width: 10),
+                child: const Icon(Icons.refresh),
+              ),
+              const SizedBox(width: 10),
 
-          FloatingActionButton(
-            heroTag: "increment",
-            backgroundColor: Colors.green,
-            onPressed: () {
-              setState(() {
-                controller.increment();
-              });
-            },
-            child: const Icon(Icons.add),
+              FloatingActionButton(
+                heroTag: "increment",
+                backgroundColor: Colors.green,
+                onPressed: () {
+                  setState(() {
+                    controller.increment();
+                  });
+                },
+                child: const Icon(Icons.add),
+              ),
+            ],
           ),
-        ],
+        ),
       ),
     );
   }
